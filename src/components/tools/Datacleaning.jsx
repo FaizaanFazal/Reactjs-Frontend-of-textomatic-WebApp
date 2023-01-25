3b98bf880f86f0b6fecf82f9f53e429319487624
ZidiKhan <dkfaizaan12@gmail.com>
2023-01-25 17:27:08 +0500

    Data cleaning update

--- src/components/tools/Datacleaning.jsx
+++ src/components/tools/Datacleaning.jsx
@@ -7,31 +7,74 @@ import { axiosInstance } from '../../config';
 export const Datacleaning = () => {
 
     const [file, setFile] = useState();
+    const [opts,setOpts]=useState([]);
+    const [pathslist,setPathslist]=useState({})
     const [filepath, setFilepath] = useState("");
     const [fileName, setFileName] = useState("");
     let p='';
-    //keep file name and file on change
-    const saveFile = (e) => {
-        setFile(e.target.files[0]);
+ 
+    //upload file
+    const UploadNow=async()=>{
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("fileName", fileName);
+      try {
+        const res= await axiosInstance.post("/Datacleaning/upload",formData)
+        p = res.data.toString();
+        p=p.replace(/(\r\n|\n|\r)/gm, "");
+        p=p.replace(/\\/g, '/');
+        console.log("response was :",p);
+        
+        setFilepath(p)
+        console.log(filepath);
+      
+      } catch (ex) {
+        console.log(ex);
+      }
+    }
+    const saveFile = async(e) => {
+       setFile(e.target.files[0]);
         setFileName(e.target.files[0].name);
+       
+      
       };
-
+      const handleCheckboxChange=(e)=>{
+            if(e.target.checked){
+              setOpts([...opts, e.target.value]);
+            
+            }
+            else{
+              setOpts(opts.filter((item) => item !== e.target.value));
+             
+            }
+            console.log(opts)
+      }
       //upload to serveer
-      const uploadFile = async (e) => {
-        const formData = new FormData();
-        formData.append("file", file);
-        formData.append("fileName", fileName);
-        try {
-          const res= await axiosInstance.post("/Datacleaning/upload",formData)
-          p = res.data.toString();
-          p=p.replace(/(\r\n|\n|\r)/gm, "");
-          console.log("response ayaa g ",p);
+      const Apply = async (e) => {
+      
+       opts.forEach((item) => {
+          console.log(item)
+          console.log("file path is ",filepath)
+        
+          try {
           
-          setFilepath(p)
-        } catch (ex) {
-          console.log(ex);
-          setFilepath(ex)
-        }
+            axiosInstance.post("/Datacleaning/upload/"+item+"?param1="+ filepath.replace(/\\/g, '/')).then(
+              res=>{ p = res.data.toString();
+                console.log("aya"+res.data)
+               p=p.replace(/(\r\n|\n|\r)/gm, "");
+          
+            setPathslist({ ...pathslist, [item]: p });
+          }
+            )
+            
+           
+           
+          } catch (ex) {
+            console.log(ex);
+            setFilepath(ex)
+          }
+        })
+       
       };
       
       const handleDownload = async () => {
@@ -62,73 +105,94 @@ export const Datacleaning = () => {
     <div className='bordergraylight text-center'>
     <div>
     <input type="file" onChange={saveFile} />
+    <button className="btn btn-outline-primary rounded-pill px-5" onClick={UploadNow}>Upload</button>   
     </div>
     <div className='row m-2'>
       <div className='col-sm-4 col-md-3'>
-      <div class="input-group mb-3">
-                  <div class="input-group-prepend">
-                    <div class="input-group-text">
-                        <input type="checkbox" aria-label="Checkbox for following text input" />
+      <div className="input-group mb-3">
+                  <div className="input-group-prepend">
+                    <div className="input-group-text">
+                        <input type="checkbox" aria-label="Checkbox for following text input" value="binary"
+                        onChange={(e)=>handleCheckboxChange(e)} />
                    </div>
                   </div>
-                  <label  class="form-control" aria-label="Text input with checkbox"> Apply Binary</label>
+                  <label  className="form-control" aria-label="Text input with checkbox"> Apply Binary</label>
                
             </div>
       </div>
       <div className='col-sm-4 col-md-3'>
-      <div class="input-group mb-3">
-                  <div class="input-group-prepend">
-                    <div class="input-group-text">
-                        <input type="checkbox" aria-label="Checkbox for following text input" />
+      <div className="input-group mb-3">
+                  <div className="input-group-prepend">
+                    <div className="input-group-text">
+                        <input type="checkbox" aria-label="Checkbox for following text input"
+                        value="entropy"
+                        onChange={(e)=>handleCheckboxChange(e)} />
                    </div>
                   </div>
-                  <label  class="form-control" aria-label="Text input with checkbox"> Entropy</label>
-               
-            </div>
-      </div>
-
-      <div className='col-sm-4 col-md-3'>
-      <div class="input-group mb-3">
-                  <div class="input-group-prepend">
-                    <div class="input-group-text">
-                        <input type="checkbox" aria-label="Checkbox for following text input" />
-                   </div>
-                  </div>
-                  <label  class="form-control" aria-label="Text input with checkbox"> TFC-Term Frequency Collection</label>
+                  <label  className="form-control" aria-label="Text input with checkbox">Entropy</label>
                
             </div>
       </div>
 
       <div className='col-sm-4 col-md-3'>
-      <div class="input-group mb-3">
-                  <div class="input-group-prepend">
-                    <div class="input-group-text">
-                        <input type="checkbox" aria-label="Checkbox for following text input" />
+      <div className="input-group mb-3">
+                  <div className="input-group-prepend">
+                    <div className="input-group-text">
+                        <input type="checkbox" aria-label="Checkbox for following text input" 
+                          value="tfc"
+                          onChange={(e)=>handleCheckboxChange(e)}
+                        />
                    </div>
                   </div>
-                  <label  class="form-control" aria-label="Text input with checkbox">LTC-Length Term Frequency</label>
+                  <label  className="form-control" aria-label="Text input with checkbox"> TFC</label>
+               
+            </div>
+      </div>
+
+      <div className='col-sm-4 col-md-3'>
+      <div className="input-group mb-3">
+                  <div className="input-group-prepend">
+                    <div className="input-group-text">
+                        <input type="checkbox" aria-label="Checkbox for following text input" 
+                          value="LTC"
+                          onChange={(e)=>handleCheckboxChange(e)}
+                        />
+                   </div>
+                  </div>
+                  <label  className="form-control" aria-label="Text input with checkbox">LTC</label>
                
             </div>
       </div>
       <div className='col-sm-4 col-md-3'>
-      <div class="input-group mb-3">
-                  <div class="input-group-prepend">
-                    <div class="input-group-text">
-                        <input type="checkbox" aria-label="Checkbox for following text input" />
+      <div className="input-group mb-3">
+                  <div className="input-group-prepend">
+                    <div className="input-group-text">
+                        <input type="checkbox" aria-label="Checkbox for following text input"
+                        value="tfidf"
+                        onChange={(e)=>handleCheckboxChange(e)} />
                    </div>
                   </div>
-                  <label  class="form-control" aria-label="Text input with checkbox">TFIDF-Term Frequency Inverse Document</label>
+                  <label  className="form-control" aria-label="Text input with checkbox">TFIDF</label>
                
             </div>
       </div>
     </div>
-    <button onClick={uploadFile}>Apply</button>    
+
+    <button className="btn btn-outline-primary rounded-pill px-5" onClick={Apply}>Apply</button>    
     
     <div>{filepath}</div>
           {filepath ? 
-            <button onClick={handleDownload}>Download</button>
-         
-          :<div>result:</div>}
+            <button onClick={handleDownload}>Download</button>         
+          :<div></div>}
+
+         <div className='row m-2'>
+         { pathslist? Object.entries(pathslist).map(([key, value]) => {
+                    return <div className='col-sm-4 col-md-3' key={key}> {key}: {value}</div>
+                }):<div></div>  
+            }
+                 
+         </div> 
+
     </div>
     </div>
     
