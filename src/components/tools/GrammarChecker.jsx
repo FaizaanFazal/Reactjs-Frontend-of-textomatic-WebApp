364a3984dac0212272415c21db52704d51e1b37d
Jalil Asif <jalilasif146@gmail.com>
2023-02-13 11:46:32 +0500

    grammarchecker done

--- src/components/tools/GrammarChecker.jsx
+++ src/components/tools/GrammarChecker.jsx
@@ -20,23 +20,42 @@ const GrammarChecker = () => {
     // Perform the grammar check on the input text and file
     setOutput(performGrammarCheck(input, file));
   };
-
+//toast appearance
+    // toast.success("", {
+    //   position: "top-right",
+    //   autoClose: 5000,
+    //   hideProgressBar: false,
+    //   closeOnClick: true,
+    //   pauseOnHover: true,
+    //   draggable: true,
+    //   progress: undefined,
+    // });
    const Apply = async (e) => {
-    toast.info('Processing Please Wait...');
+    toast('Processing Please Wait...');
     let para="";
+    let para2="";
       if(file){
         para="file";
+        para2=filepath.replace(/\\/g, '/');
       }
       else{
         para="text"
+        para2=input;
       }
           console.log("Applying on ",filepath)
           try {
-            axiosInstance.post("/grammar?param1="+ filepath.replace(/\\/g, '/')+"&param2="+para).then(
+            axiosInstance.post("/grammar?param1="+para2 +"&param2="+para).then(
               res=>{ p = res.data.toString();
               console.log("aya"+res.data)
-              p=p.replace(/(\r\n|\n|\r)/gm, "");
-              setOutputfile(p);
+             
+              if(para=="text"){
+                setOutput(p)
+              }
+              else{
+                p=p.replace(/(\r\n|\n|\r)/gm, "");
+                setOutputfile(p);
+              }
+             
           })         
           } catch (ex) {
             console.log(ex);
@@ -79,20 +98,21 @@ const GrammarChecker = () => {
 const UploadNow= async()=>{
   
   console.log("uploading")
-  const formData = new FormData();
-    formData.append("file", file);
-    formData.append("fileName", fileName);
+  const formData2 = new FormData();
+    formData2.append("file", file);
+    formData2.append("fileName", fileName);
     try {
-      await axiosInstance.post("/Datacleaning/upload",formData).then(res=>{
+    const res=  await axiosInstance.post("/Datacleaning/upload",formData2)
         p = res.data.toString(); // file url
         p=p.replace(/(\r\n|\n|\r)/gm, "");
         p=p.replace(/\\/g, '/');
         console.log("response was :",p);
-        toast.success('File uploaded succesfully.');
+        toast('File uploaded succesfully.');
         setFilepath(p)
-      })
+      
     } catch (ex) {
       console.log(ex);
+      toast('Something went Wrong!!');
     }
     
 }
@@ -100,7 +120,7 @@ const UploadNow= async()=>{
  
     setLocalpath(outputfile)
 
-},[outputfile])
+},[outputfile,output])
 
   return (
   <div className='container text-black mb-5 text-center'> 
@@ -123,15 +143,24 @@ const UploadNow= async()=>{
       </div>
       <div className="col-md-6 hide">
           
-          <div class="form-outline output position-relative">
+         { output ? <div class="form-outline output position-relative">
               <textarea disabled class="form-control" id="textarea" rows="7"
-              value="Output here"
+              value={output}
               onChange={(e) => setInput(e.target.value)}
               text=""
               ></textarea>
               <button className="pos-rt btn rounded m-0 p-2 ">C</button>
            
-            </div>
+            </div>:
+            <div class="form-outline output position-relative">
+              <textarea disabled class="form-control" id="textarea" rows="7"
+              value="output here"
+              onChange={(e) => setInput(e.target.value)}
+              text=""
+              ></textarea>
+              <button className="pos-rt btn rounded m-0 p-2 ">C</button>
+           
+            </div>}
           </div>
       
     </div>
@@ -145,15 +174,15 @@ const UploadNow= async()=>{
    
         {/* <label class="custom-file-label" for="customFile">Choose file in txt format</label> */}
       </div>
-      <button disabled={!file || !input} onClick={Apply} className="btn rounded-pill px-5 bg-gradient-purple">Apply</button>
+      <button disabled={!filepath && !input} onClick={Apply} className="btn rounded-pill px-5 bg-gradient-purple">Apply</button>
 
         {/* download */}
       <div className='row m-2'>
-        { outputfile ? <div className="col-sm-12 py-5 text-center">
+        { localpath ? <div className="col-sm-12 py-5 text-center">
             <h3 className='text-center'>Download Suggested correction file </h3>
             <button className='btn btn-success rounded-pill px-5' 
             onClick={(e)=>handleDownload(e.target.value)} 
-            value={outputfile}
+            value={localpath}
             > Result</button>
             </div>:<div></div>
         }
